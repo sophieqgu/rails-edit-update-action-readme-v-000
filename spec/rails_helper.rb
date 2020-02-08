02ca44c966e64d95d19e83f976195d7da055756f
Sophie Gu <sophieqgu@gmail.com>
2020-02-08 22:36:12 +0000

    Automatically backed up by Learn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,4 +35,23 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = false
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
